df68f1b3b9ab2c811ab562dd64dc63da35ccbc76
kr-devesh-0246 <devesh2462003@gmail.com>
2023-07-21 09:43:13 +0530

    pushing changes

---
+++ src/patterns/QuickSort.java
@@ -0,0 +1,43 @@
+package patterns;
+
+import java.util.Arrays;
+
+public class QuickSort {
+    private static void sort(int[] arr, int low, int high) {
+        if (low >= high) {
+            return;
+        }
+
+        int s = low;
+        int e = high;
+        int m = s + (e - s) / 2;
+        int pivot = arr[m];
+
+        while (s <= e) {
+            while (arr[s] < pivot) {
+                s++;
+            }
+            while (arr[e] > pivot) {
+                e--;
+            }
+
+            // the reason why if its already sorted it will not wap. Hence it is better than Merge sort.
+            if (s <= e) {
+                int temp = arr[s];
+                arr[s] = arr[e];
+                arr[e] = temp;
+                s++;
+                e--;
+            }
+        }
+
+        sort(arr, low, e);
+        sort(arr, s, high);
+    }
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 8, 7, 6, 5, 4, 3, 0};
+        sort(arr, 0, arr.length-1);
+        System.out.println(Arrays.toString(arr));
+    }
+
+}
