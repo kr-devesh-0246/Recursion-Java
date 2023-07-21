667ea94cb81838d718fb02bb00e7da93d525f8c3
kr-devesh-0246 <devesh2462003@gmail.com>
2023-07-21 10:22:58 +0530

    pushing changes

--- src/patterns/SelectionSort.java
+++ src/patterns/SelectionSort.java
@@ -3,24 +3,26 @@ package patterns;
 import java.util.Arrays;
 
 public class SelectionSort {
-    public static void selection(int[] arr, int r, int c, int max) {
-        // base-case
+    private static void selection(int[] arr, int r, int c, int max) {
+        // max is the index of max element
         if (r == 0) {
             return;
         }
         if (c < r) {
-            if ((arr[c] > arr[max])) {
-                selection(arr, r, c + 1, c);
+            if (arr[c] > arr[max]) {
+                selection(arr, r, c+1, c);
             } else {
-                selection(arr, r, c + 1, max);
+                selection(arr, r, c+1, max);
             }
         } else {
+            // update the new max element
             int temp = arr[max];
             arr[max] = arr[r-1];
             arr[r-1] = temp;
 
             selection(arr, r-1, 0, 0);
         }
+
     }
     public static void main(String[] args) {
         int[] arr = {9,8,7,6,5,4,3,1};
