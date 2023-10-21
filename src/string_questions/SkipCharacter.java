7226a5fc46e4c37c58e42f3843b145dbfaf49183
kr-devesh-0246 <devesh2462003@gmail.com>
2023-10-21 15:51:25 +0530

    added two questions

---
+++ src/string_questions/SkipCharacter.java
@@ -0,0 +1,42 @@
+package string_questions;
+
+public class RemoveCharacter {
+    public static void main(String args[]) {
+        String s = "baccad";
+        System.out.println("Using void returning func");
+        skipChar("", s);
+
+        System.out.println("Using String returning func");
+        String p = skipChar("baccad");
+        System.out.println(p);
+    }
+
+    private static void skipChar(String processed, String unprocessed) {
+        if (unprocessed.isEmpty()) {
+            System.out.println(processed);
+            return ;
+        }
+
+        char ch = unprocessed.charAt(0);
+
+        if (ch == 'a') {
+            skipChar(processed, unprocessed.substring(1));
+        } else {
+            skipChar(processed + ch, unprocessed.substring(1));
+        }
+    }
+
+    private static String skipChar(String unprocessed) {
+        if (unprocessed.isEmpty()) {
+            return "";
+        }
+
+        char ch = unprocessed.charAt(0);
+
+        if (ch == 'a') {
+            return skipChar(unprocessed.substring(1));
+        } else {
+            return ch + skipChar(unprocessed.substring(1));
+        }
+    }
+}
