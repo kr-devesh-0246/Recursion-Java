94465b35eaa93ba9826e1127dfa37b75f0d1d166
kr-devesh-0246 <devesh2462003@gmail.com>
2023-10-25 11:56:44 +0530

    adding maze problem file

--- src/backtracking/Maze.java
+++ src/backtracking/Maze.java
@@ -4,11 +4,19 @@ import java.util.ArrayList;
 
 public class Maze {
     public static void main(String[] args) {
+        // returns the number of paths
         System.out.println(countPaths(3, 3));
-        path("",  3, 3);
-        System.out.println(pathRet("", 3, 3));
-    }
 
+        // prints the paths
+        path("",  3, 3);
+
+        //returns the paths in arraylist
+        System.out.println(pathRet("", 3, 3));
+
+        // returns the paths with diagonal movements in arraylist
+        System.out.println(pathRetDiagonal("", 3, 3));
+
+    }
     static int countPaths(int r, int c) {
         if (r == 1 || c == 1) {
             return 1;
@@ -53,5 +61,30 @@ public class Maze {
 
         return list;
     }
+
+    // Now we can move along the diagonal too.
+    static ArrayList<String> pathRetDiagonal(String p, int r, int c) {
+        if (r == 1 && c == 1) {
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+
+        // local list
+        ArrayList<String> list = new ArrayList<>();
+
+        if (r > 1 && c > 1) {
+            list.addAll(pathRetDiagonal(p + 'D', r-1, c-1));
+        }
+        if (r > 1) {
+            list.addAll(pathRetDiagonal(p + 'V', r-1, c));
+        }
+        if (c > 1) {
+            list.addAll(pathRetDiagonal(p + 'H', r, c-1));
+        }
+
+        return list;
+    }
+
 
 }
