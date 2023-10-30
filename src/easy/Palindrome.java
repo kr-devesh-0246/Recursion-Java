2a7fabcbe31bc93e1cd6a530ccf90a6b39ab23e9
kr-devesh-0246 <devesh2462003@gmail.com>
2023-10-30 15:34:55 +0530

    solved palindrome problem recursively

--- src/easy/Palindrome.java
+++ src/easy/Palindrome.java
@@ -2,6 +2,23 @@ package easy;
 
 public class Palindrome {
     public static void main(String args[]) {
-        
+        String s = "abba";
+        System.out.println(isPalindrome(s));
+    }
+
+    static boolean isPalindrome(String s) {
+        // base-case
+        if (s.length() <= 1) {
+            return true;
+        }
+        char head = s.charAt(0);
+        char tail = s.charAt(s.length() - 1);
+
+        if (head == tail){
+            String middle = s.substring(1, s.length() - 1);
+            return isPalindrome(middle);
+        } else {
+            return false;
+        }
     }
 }
